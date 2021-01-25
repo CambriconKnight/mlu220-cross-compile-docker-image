29af3533ac0b8e7f522dcd72719f0f6901a52af7
kang <kangshaopeng@cambricon.com>
2021-01-25 22:59:40 +0800

    init

---
+++ build-mlu220-cross-compile-image.sh
@@ -0,0 +1,187 @@
+#/bin/bash
+set -e
+# -------------------------------------------------------------------------------
+# Filename:     build-mlu220-cross-compile-image.sh
+# UpdateDate:   2021/01/25
+# Description:  Build docker images for mlu220-cross-compile.
+# Example:      
+# Depends:
+# Notes:        
+# -------------------------------------------------------------------------------
+# Version 1.0.0
+# Initial version
+# -------------------------------------------------------------------------------
+#################### function ####################
+help_info() {
+    echo "
+Build docker images for mlu220-cross-compile.
+Usage:
+    $0 <command> [arguments]
+The commands are:
+    -h      Help info.
+    -m      MLU Platform.(mlu270, mlu220m.2)
+    -v      Neuware version.
+    -n      FLAG_with_neuware_installed.(0/1;default:0)
+    -l      FLAG_with_gcc_linaro_installed.(0/1;default:1)
+    -a      FLAG_with_gcc_arm_installed.(0/1;default:0)
+Examples:
+    $0 -h
+    $0 -m mlu270 -v 1.5.0
+    $0 -m mlu220m.2 -v 1.5.0
+Use '$0 -h' for more information about a command.
+    "
+}
+
+# Refresh global variables
+refresh_global_variables() {
+    MLU_Platform=`echo ${MLU} | tr '[a-z]' '[A-Z]'`
+    if [[ "${MLU_Platform}" == "MLU270" ]] || [[ "${MLU_Platform}" == "MLU220M.2" ]] ; then
+        MLU_Platform="MLU270"
+    else
+        help_info
+    fi
+    VERSION="v${VER}"
+    PATH_WORK="mlu220-cross-compile"
+    neuware_version="neuware-${MLU}-${VER}"
+    neuware_package_name="${neuware_version}-1_Ubuntu16.04_amd64.deb"
+    NAME_IMAGE="ubuntu16.04_mlu220-cross-compile:$VERSION"
+    FILENAME_IMAGE="ubuntu16.04_mlu220-cross-compile-$VERSION.tar.gz"
+}
+
+#################### main ####################
+#MLU Platform
+MLU="mlu270"
+#Version
+VER="1.5.0"
+
+#Global variables
+#UPPERCASE:mlu270--->MLU270
+MLU_Platform=`echo ${MLU} | tr '[a-z]' '[A-Z]'`
+VERSION="v${VER}"
+PATH_WORK="mlu220-cross-compile"
+NAME_IMAGE="ubuntu16.04_$PATH_WORK:$VERSION"
+FILENAME_IMAGE="ubuntu16.04_$PATH_WORK-$VERSION.tar.gz"
+#neuware
+neuware_version="neuware-${MLU}-${VER}"
+neuware_package_name="${neuware_version}-1_Ubuntu16.04_amd64.deb"
+#gcc-linaro
+FILENAME_MLU220_GCC_LINARO="gcc-linaro-6.2.1-2016.11-x86_64_aarch64-linux-gnu.tgz"
+#gcc-arm-none-eabi
+FILENAME_MLU220_GCC_ARM="gcc-arm-none-eabi-8-2018-q4-major.tar.gz"
+
+##FLAG_with_***_installed
+FLAG_with_neuware_installed=0
+FLAG_with_gcc_linaro_installed=1
+FLAG_with_gcc_arm_installed=0
+FLAG_with_neuware_installed2dockerfile="no"
+FLAG_with_gcc_linaro_installed2dockerfile="yes"
+FLAG_with_gcc_arm_installed2dockerfile="no"
+
+none="\033[0m"
+green="\033[0;32m"
+red="\033[0;31m"
+yellow="\033[1;33m"
+
+if [[ $# -eq 0 ]];then
+    help_info && exit 0
+fi
+
+# Get parameters
+while getopts "h:m:v:n:l:a:" opt; do
+    case $opt in
+    h) help_info  &&  exit 0
+        ;;
+    m) MLU="$OPTARG"
+        ;;
+    v) VER="$OPTARG"
+        ;;
+    n) FLAG_with_neuware_installed=$OPTARG
+        ;;
+    l) FLAG_with_gcc_linaro_installed=$OPTARG
+        ;;
+    a) FLAG_with_gcc_arm_installed=$OPTARG
+        ;;
+    \?)
+        help_info && exit 0
+        ;;
+    esac
+done
+
+# Refresh global variables
+refresh_global_variables
+
+## 0.check
+if [ ! -d "$PATH_WORK" ];then
+    mkdir -p $PATH_WORK
+else
+    echo "Directory($PATH_WORK): Exists!"
+fi
+cd "${PATH_WORK}"
+
+## copy the dependent packages into the directory of $PATH_WORK
+##copy your neuware package into the directory
+if [ $FLAG_with_neuware_installed -eq 1 ];then
+    if [ -f "${neuware_package_name}" ];then
+        echo "File(${neuware_package_name}): Exists!"
+    else
+        echo -e "${red}File(${neuware_package_name}): Not exist!${none}"
+        echo -e "${yellow}Copy the dependent packages(${neuware_package_name}) into the directory!${none}"
+        echo -e "${yellow}eg:cp -v /data/ftp/$VERSION/neuware/${neuware_package_name} ./${PATH_WORK}${none}"
+        #Manual copy
+        #cp -v /data/ftp/v1.5.0/neuware/neuware-mlu270-1.5.0-1_Ubuntu16.04_amd64.deb ./cnstream
+        exit -1
+    fi
+    FLAG_with_neuware_installed2dockerfile="yes"
+else
+    FLAG_with_neuware_installed2dockerfile="no"
+fi
+
+### FILENAME_MLU220_GCC_LINARO(ftp://download.cambricon.com:8821/product/MLU220/交叉编译工具/aarch64/gcc-linaro-6.2.1-2016.11-x86_64_aarch64-linux-gnu.tgz)
+if [ $FLAG_with_gcc_linaro_installed -eq 1 ];then
+    if [ -f "${FILENAME_MLU220_GCC_LINARO}" ];then
+        echo "File(${FILENAME_MLU220_GCC_LINARO}): Exists!"
+    else
+        echo -e "${red}File(${FILENAME_MLU220_GCC_LINARO}): Not exist!${none}"
+        echo -e "${yellow}Copy the dependent packages(${FILENAME_MLU220_GCC_LINARO}) into the directory!${none}"
+        echo -e "${yellow}eg:cp -v /data/ftp/mlu220/cross-compile-toolchain/aarch64/${FILENAME_MLU220_GCC_LINARO} ./${PATH_WORK}${none}"
+        #Manual copy
+        #cp -v /data/ftp/mlu220/cross-compile-toolchain/aarch64/gcc-linaro-6.2.1-2016.11-x86_64_aarch64-linux-gnu.tgz ./mlu220-cross-compile
+        exit -1
+    fi
+    FLAG_with_gcc_linaro_installed2dockerfile="yes"
+else
+    FLAG_with_gcc_linaro_installed2dockerfile="no"
+fi
+
+### FILENAME_MLU220_GCC_ARM(ftp://download.cambricon.com:8821/product/MLU220/交叉编译工具/aarch64/gcc-arm-none-eabi-8-2018-q4-major.tar.gz)
+if [ $FLAG_with_gcc_arm_installed -eq 1 ];then
+    if [ -f "${FILENAME_MLU220_GCC_ARM}" ];then
+        echo "File(${FILENAME_MLU220_GCC_ARM}): Exists!"
+    else
+        echo -e "${red}File(${FILENAME_MLU220_GCC_ARM}): Not exist!${none}"
+        echo -e "${yellow}Copy the dependent packages(${FILENAME_MLU220_GCC_ARM}) into the directory!${none}"
+        echo -e "${yellow}eg:cp -v /data/ftp/mlu220/cross-compile-toolchain/m0/${FILENAME_MLU220_GCC_ARM} ./${PATH_WORK}${none}"
+        #Manual copy
+        #cp -v /data/ftp/mlu220/cross-compile-toolchain/aarch64/gcc-arm-none-eabi-8-2018-q4-major.tar.gz ./mlu220-cross-compile
+        exit -1
+    fi
+    FLAG_with_gcc_arm_installed2dockerfile="yes"
+else
+    FLAG_with_gcc_arm_installed2dockerfile="no"
+fi
+
+#1.build image
+echo "====================== build image ======================"
+sudo docker build -f ../Dockerfile.16.04 \
+    --build-arg neuware_package=${neuware_package_name} \
+    --build-arg mlu_platform=${MLU_Platform} \
+    --build-arg with_neuware_installed=${FLAG_with_neuware_installed2dockerfile} \
+    --build-arg with_gcc_linaro_installed=${FLAG_with_gcc_linaro_installed2dockerfile} \
+    --build-arg with_gcc_arm_installed=${FLAG_with_gcc_arm_installed2dockerfile} \
+    -t $NAME_IMAGE .
+#2.save image
+echo "====================== save image ======================"
+sudo docker save -o $FILENAME_IMAGE $NAME_IMAGE
+mv $FILENAME_IMAGE ../
+cd ../
+ls -la $FILENAME_IMAGE
---
+++ run-container-mlu220-cross-compile.sh
@@ -0,0 +1,56 @@
+#/bin/bash
+set -e
+
+#Version
+VERSION="v1.5.0"
+PATH_WORK="mlu220-cross-compile"
+if [[ $# -eq 1 ]];then
+    VERSION=$1
+fi
+#Image name
+MY_IMAGES="ubuntu16.04_$PATH_WORK:$VERSION"
+#Docker container name
+MY_CONTAINER="container-$PATH_WORK-$VERSION"
+
+######Modify according to your development environment#####
+#SDK path on the host
+PATH_WORKSPACE_HOST="/data/ftp"
+#Work path on the docker container
+PATH_WORKSPACE_DOCKER="/home/ftp"
+#Work2 path on the host
+PATH_WORKSPACE2_HOST="$PWD"
+#Work2 path on the docker container
+PATH_WORKSPACE2_DOCKER="/home/cam"
+#Datasets path on the host
+PATH_DATASETS_HOST="/data/datasets"
+#Datasets path on the docker container
+PATH_DATASETS_DOCKER="/data/datasets"
+#Models path on the host
+PATH_MODELS_HOST="/data/models"
+#Models path on the docker container
+PATH_MODELS_DOCKER="/data/models"
+#Pro path on the host
+#PATH_PRO_HOST="/data/pro"
+#Pro path on the docker container
+#PATH_PRO_DOCKER="/data/pro"
+
+##########################################################
+
+num=`sudo docker ps -a|grep -w "$MY_CONTAINER$"|wc -l`
+
+echo $num
+echo $MY_CONTAINER
+
+if [ 0 -eq $num ];then
+    #sudo xhost +
+    sudo docker run -e DISPLAY=unix$DISPLAY --privileged=true \
+        --net=host --pid=host --ipc=host -v /tmp/.X11-unix:/tmp/.X11-unix \
+        -it -v $PATH_WORKSPACE_HOST:$PATH_WORKSPACE_DOCKER \
+        -it -v $PATH_WORKSPACE2_HOST:$PATH_WORKSPACE2_DOCKER \
+        -it -v $PATH_DATASETS_HOST:$PATH_DATASETS_DOCKER \
+        -it -v $PATH_MODELS_HOST:$PATH_MODELS_DOCKER \
+        --name $MY_CONTAINER $MY_IMAGES /bin/bash
+else
+    sudo docker start $MY_CONTAINER
+    sudo docker exec -ti $MY_CONTAINER /bin/bash
+fi
