938ad19ef810d763552aa5e402806109be4bd800
kang <kangshaopeng@cambricon.com>
2021-01-26 12:37:38 +0800

    add device-tree-compiler&libssl-dev&bc

--- build-mlu220-cross-compile-image.sh
+++ build-mlu220-cross-compile-image.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 set -e
 # -------------------------------------------------------------------------------
 # Filename:     build-mlu220-cross-compile-image.sh
--- load-image-mlu220-cross-compile.sh
+++ load-image-mlu220-cross-compile.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 set -e
 # -------------------------------------------------------------------------------
 # Filename:     load-image-mlu220-cross-compile.sh
