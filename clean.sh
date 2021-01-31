6139e22eb3b231ffee24faa825c95c6c49a84676
kang <kangshaopeng@cambricon.com>
2021-01-31 15:11:42 +0800

    add clean.sh

---
+++ clean.sh
@@ -0,0 +1,27 @@
+#/bin/bash
+set -e
+
+VER="1.6.0"
+VERSION="v${VER}"
+PATH_WORK="mlu220-cross-compile"
+NAME_IMAGE="ubuntu16.04_$PATH_WORK:$VERSION"
+FILENAME_IMAGE="ubuntu16.04_$PATH_WORK-$VERSION.tar.gz"
+#Docker container name
+MY_CONTAINER="container-$PATH_WORK-$VERSION"
+#Docker image
+MY_IMAGE="ubuntu16.04_$PATH_WORK"
+
+sudo rm -rvf ${PATH_WORK}
+sudo rm -vf ${FILENAME_IMAGE}
+
+#rm docker container
+#sudo docker stop `sudo docker ps -a | grep container-mlu220-cross-compile | awk '{print $1}'`
+num_container=`sudo docker ps -a | grep ${MY_CONTAINER} | awk '{print $1}'`
+if [ $num_container ]; then sudo docker stop $num_container;fi
+#sudo docker rm `sudo docker ps -a | grep container-mlu220-cross-compile | awk '{print $1}'`
+if [ $num_container ]; then sudo docker rm $num_container;fi
+
+#rmi docker image
+#sudo docker rmi `sudo docker images | grep ubuntu16.04_mlu220-cross-compile | awk '{print $3}'`
+num_images=`sudo docker images | grep ${MY_IMAGE} | awk '{print $3}'`
+if [ $num_images ]; then sudo docker rmi $num_images;fi
